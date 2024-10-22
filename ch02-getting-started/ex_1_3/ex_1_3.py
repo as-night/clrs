2ef8c4a26c3d3a3c08f0d3aabd5576f58d9956de
as-night <as-night@users.noreply.github.com>
2024-10-22 10:46:25 +0200

    Add an implementation and proof of the correctness for the linear search

---
+++ ch02-getting-started/ex_1_3/ex_1_3.py
@@ -0,0 +1,32 @@
+'''Linear search
+
+A simple, proof-of-concept implementation of linear search.
+'''
+
+__all__ = ['linear_search']
+
+from typing import List, Optional
+
+# We're running on macOS' 3.9.6 by default, so no support for the |
+# operator in function signatures.
+def linear_search(a: List[int], key: int) -> Optional[int]:
+    '''Return the index of the first occcurence of a key or None.'''
+    for index, value in enumerate(a):
+        if value == key:
+            return index
+    return None
+
+def main():
+    l = [31, 41, 59, 26, 41, 58]
+    key = 22
+    print(f'list: {l}',
+          f'key: {key}',
+          sep='\n')
+    ret = linear_search(l, key)
+    if ret:
+        print(f'Found the key {key} at the index {ret}.')
+    else:
+        print(f'Found {ret}.')
+
+if __name__ == '__main__':
+    raise SystemExit(main())
